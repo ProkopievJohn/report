ee9e02304d95ce785b972347b0c9d4cf56e3232b
ProkopievJohn <ProkopievJohn@gamil.com>
2017-06-19 01:15:29 +0300

    first config webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log('aaaaaaaaaaaaaaaaaaaaaaaa');
+console.log('aaaa');
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,61 @@
-const debug = process.env.NODE_ENV !== "production";
-const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: __dirname + "/dist/",
-		filename: "bundle.js",
-		publicPath: '/',
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js/,
-				exclude: /node_modules/,
-				include: path.resolve(__dirname, "src"),
-				loader: 'babel-loader',
-				query: {
-					presets: ['react', 'env'],
-				}
-			}
+const paths = {
+	src: path.join(__dirname, 'src'),
+	dist: path.join(__dirname, 'dist'),
+}
+
+const common = merge([
+	{
+		entry: paths.src + '/index.js',
+		output: {
+			path: paths.dist,
+			filename: '[name].js',
+		},
+		module: {
+			loaders: [
+				// {
+				// 	test: /\.js$/,
+				// 	loader: 'babel-loader',
+				// 	query: {
+				// 		presets: ['react', 'env'],
+				// 	}
+				// },
+				{
+					test: /\.jade$/,
+					loader: 'jade-loader',
+					options: {
+						pretty: true,
+					},
+				},
+			]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				filename: 'index.html',
+				template: paths.src + '/index.jade',
+			}),
 		],
-	},
-	plugins: debug ? [] : [],
+	}
+]);
+
+module.exports = function(env) {
+	if (env === 'development') {
+		return merge([
+            common,
+			{
+				devServer: {
+					stats: 'errors-only',
+					port: 3000,
+				},
+			}
+        ]);
+	}
+	else if (env === 'production') {
+		return merge([
+            common,
+        ]);
+	}
 };
