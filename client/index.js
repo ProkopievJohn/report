5e1bfd688553259dc914ed8406411a21a8d2bee7
Ivan Devico <ivan.prokopiev@devico.io>
2017-07-19 17:28:14 +0300

    start with create-react-app

--- client/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
-import store from './state';
-import { Provider } from 'react-redux';
-
-render(<Provider store={store}><App /></Provider>, document.querySelector('#react-app'));
