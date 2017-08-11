bef167c58b4efc2704cf1df22753fd857ece772b
Ivan Devico <ivan.prokopiev@devico.io>
2017-08-11 20:18:47 +0300

    temp

---
+++ __tests__/src/components/user/Login.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import Login from '../../../../src/components/user/Login'
+
+describe('Login', () => {
+  let subject;
+
+  beforeEach(() => {
+    subject = TestUtils.renderIntoDocument(<Login/>);
+  });
+
+  it('has elements for login form', () => {
+    expect(subject.refs.submitBtn.innerText).toEqual('Login');
+    expect(subject.refs.emailTxt.placeholder).toEqual('email');
+    expect(subject.refs.passwordTxt.placeholder).toEqual('password');
+    expect(subject.refs.passwordTxt.type).toEqual('password');
+  });
+});
+
